32ce824b0994f8198d7de82edb240a8d16761190
ZèD <imzihad@gmail.com>
2020-10-07 20:05:43 +0600

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -3,11 +3,11 @@
 git clone https://${GITAUTH}@github.com/n00b-Dev-Stuffs/google-git-cookies.git
 bash google-git-cookies/setup_cookies.sh;rm -rf google-git-cookies
 
-repo init -u https://github.com/Evolution-X/manifest -b ten  --depth 1 --groups all,-notdefault,-device,-darwin,-mips
+repo init -u https://github.com/AOSPA/manifest -b quartz --depth 1 --groups all,-notdefault,-device,-darwin,-mips
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 export USE_CCACHE=1
 ccache -M 20G
-source build/envsetup.sh && lunch aosp_sakura-user && mka bacon -j$(nproc --all)
+source rom-build.sh sakura
 curl -sL https://git.io/file-transfer | sh
 ./transfer wet out/target/product/sakura/Evolution*
